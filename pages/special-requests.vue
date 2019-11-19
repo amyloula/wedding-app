47cdabea07f04f0df6158ab3a2550e3a04c45759
amyloula <mccabeamylouise@gmail.com>
2019-11-19 19:49:43 +0000

    feature(pages) special-requests with spotify integ

--- pages/special-requests.vue
+++ pages/special-requests.vue
@@ -1,13 +1,26 @@
 <template>
-    
+  <div class="requests">
+    <h1>Special requests</h1>
+    <iframe
+      src="https://open.spotify.com/embed/playlist/6wtTYCAnnJtbnKpSFXxnbS"
+      width="300"
+      height="380"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
+@import '~assets/styles/_variables.scss';
 
+.requests {
+  text-align: center;
+  font-family: $secondary-header-font;
+}
 </style>
